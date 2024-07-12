6147757769c74165744bd8d8198cc3c4b45d81fd
MTChaoyi <MTChaoyi@outlook.com>
2024-07-12 10:53:53 +0800

    perf: 优化首页流云效果 & 更换网站域名

--- source/js/custom-1.js
+++
@@ -1,7 +0,0 @@
-var container = document.querySelector("body > main > div.first-screen-container.border-box.flex-center.fade-in-down-animation");
-
-if (container) {
-    var cloudDiv = document.createElement('div');
-    cloudDiv.className = 'bg-cloud';
-    container.appendChild(cloudDiv);
-}
