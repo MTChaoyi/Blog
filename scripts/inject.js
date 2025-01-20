92f5214db3e956d278ab5b0ec8873a972dbd010d
MTChaoyi <MTChaoyi@outlook.com>
2025-01-21 02:26:00 +0800

    chore: 更新主题 & 添加 umami 分析

---
+++ scripts/inject.js
@@ -0,0 +1,5 @@
+hexo.extend.injector.register(
+  "head_end",
+  '<script defer src="https://analyze.mtian.net/script.js" data-website-id="4c85a02d-eaa4-441e-b3df-a92a7ae933cc"></script>',
+  "default"
+);
